b76da29b87d2e6794140b1d39c11628a282ebb4a
zylx <549535805@qq.com>
2020-09-05 10:33:13 +0800

    添加音乐首页轮播图

--- miniprogram/pages/palylist/index.js
+++ miniprogram/pages/palylist/index.js
@@ -5,7 +5,16 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    swiperImgUrls: [{
+        url: 'http://p1.music.126.net/oeH9rlBAj3UNkhOmfog8Hw==/109951164169407335.jpg',
+      },
+      {
+        url: 'http://p1.music.126.net/xhWAaHI-SIYP8ZMzL9NOqg==/109951164167032995.jpg',
+      },
+      {
+        url: 'http://p1.music.126.net/Yo-FjrJTQ9clkDkuUCTtUg==/109951164169441928.jpg',
+      }
+    ]
   },
 
   /**
