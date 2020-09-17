e457a264d6898f7f7eb8c93d6c1041992b395616
zylx <549535805@qq.com>
2020-09-17 22:29:53 +0800

    增加博客详情页面

--- miniprogram/pages/blog-detail/blog-detail.js
+++ miniprogram/pages/blog-detail/blog-detail.js
@@ -1,18 +1,60 @@
 // miniprogram/pages/blog-detail/blog-detail.js
+import formatTime from '../../utils/formatTime.js'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    blogId: '',
+    blog: {},
+    commentList: [],
+    likeCount: 0,
+    commentCount: 0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log(options)
+    this.setData({
+      blogId: options.blogId
+    })
+    this._getBlogDetail()
+  },
+
+  // 获取博客详情
+  _getBlogDetail() {
+    wx.showLoading({
+      title: '加载中',
+      mask: true,
+    })
+    wx.cloud.callFunction({
+      name: 'blog',
+      data: {
+        $url: 'detail',
+        blogId: this.data.blogId
+      }
+    }).then((res) => {
+      const result = res.result
+      const likeCount = result.like && result.like.length || 0
+      let commentList = result.commentList || []
+      delete result.like
+      delete result.commentList
+      if (commentList.length > 0) {
+        for (let i = 0, len = commentList.length; i < len; i++) {
+          commentList[i].createtime = formatTime(new Date(commentList[i].createtime))
+        }
+      }
+      this.setData({
+        likeCount,
+        commentList,
+        blog: result,
+        commentCount: commentList.length,
+      })
+      wx.hideLoading()
+      wx.stopPullDownRefresh()
+    })
   },
 
   /**
@@ -47,7 +89,7 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    this._getBlogDetail()
   },
 
   /**
